01c4bad2a2fa689cb85355c99807cd96942ed7c9
Matt Smith <msmithfl@icloud.com>
2023-04-25 18:47:58 -0400

    layout refactor

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,14 +5,14 @@ export default function Card() {
     <div className="flex items-center justify-between h-20 mb-1 p-2 bg-slate-200 rounded-sm">
       <div>
         <p>Skatepark Name</p>
-        <p>Distance: 15 mi.</p>
+        <p className="text-sm">Distance: 15 mi.</p>
       </div>
       <div className="flex items-center">
         <div className="flex flex-col items-center">
           <img className="w-11" alt="weather-icon" src="icons/01d.png" />
-          <p>76°F</p>
+          <p className="font-bold">76°F</p>
         </div>
-        <i className="fa-solid fa-plus text-slate-600"></i>
+        <i className="fa-solid fa-plus text-slate-600 text-xl pl-2"></i>
       </div>
     </div>
   );
--- src/components/Feed.js
+++ src/components/Feed.js
@@ -1,10 +1,9 @@
 import React from "react";
-import "../App.css";
 import Card from "./Card";
 
 export default function Feed() {
   return (
-    <div className="flex flex-col m-2">
+    <div className="flex flex-col justify-center m-2">
       <Card />
       <Card />
     </div>
