ca8a4d0f300714209ea90f38f37e38dbf8a0624d
Matt Smith <msmithfl@icloud.com>
2023-04-26 00:22:25 -0400

    end o night

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,10 +2,12 @@ import React from "react";
 
 function Footer() {
   return (
-    <div className="fixed bottom-0 w-full bg-black/[.7] z-10 p-4">
+    <div className="fixed bottom-0 w-full bg-black/[.7] z-10 p-4 rounded-t-2xl">
       <div className="flex justify-around">
         <i className="fa-solid fa-magnifying-glass text-lg"></i>
+        <p className="text-lg">|</p>
         <i className="fa-solid fa-map text-lg"></i>
+        <p className="text-lg">|</p>
         <i className="fa-solid fa-heart text-lg"></i>
       </div>
     </div>
