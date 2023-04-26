d1e1e35adc82eca829c8cd955438764045be9ad4
Matt Smith <msmithfl@icloud.com>
2023-04-25 23:24:02 -0400

    card design

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,17 +2,17 @@ import React from "react";
 
 export default function Card() {
   return (
-    <div className="flex items-center justify-between h-20 mb-1 p-2 bg-slate-200 rounded-sm">
+    <div className="flex items-center justify-between h-20 mb-1 p-2 bg-slate-100/[.3] rounded-sm">
       <div>
-        <p>Skatepark Name</p>
-        <p className="text-sm">Distance: 15 mi.</p>
+        <p className="text-black">Skatepark Name</p>
+        <p className="text-sm text-black">Distance: 15 mi.</p>
       </div>
       <div className="flex items-center">
         <div className="flex flex-col items-center">
           <img className="w-11" alt="weather-icon" src="icons/01d.png" />
-          <p className="font-bold">76°F</p>
+          <p className="font-bold text-black">76°F</p>
         </div>
-        <i className="fa-solid fa-plus text-slate-600 text-xl pl-2"></i>
+        <i className="fa-solid fa-plus text-slate-100/[.4] text-xl pl-2"></i>
       </div>
     </div>
   );
--- src/components/Feed.js
+++ src/components/Feed.js
@@ -1,12 +1,11 @@
 import React from "react";
-//import Card from "./Card";
+import Card from "./Card";
 
 export default function Feed() {
   return (
-    <div className="flex flex-col justify-center m-2">
-      Feed
-      {/* <Card />
-      <Card /> */}
+    <div className="flex flex-col justify-center m-2 pt-2">
+      <Card />
+      <Card />
     </div>
   );
 }
